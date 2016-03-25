a42733456014fc1ebe7a92f2b5297dc85d76c839
Shocking <lucas@lucascullen.com>
2016-03-25 16:28:30 +1000

    Remove code

--- BitPoker/Program.cs
+++ BitPoker/Program.cs
@@ -81,49 +81,6 @@ namespace BitPoker
 
 		}
 
-//		public static byte[] EncryptStringToBytes_Aes(string plainText, byte[] Key, byte[] IV)
-//		{
-//			// Check arguments.
-//			if (plainText == null || plainText.Length <= 0)
-//				throw new ArgumentNullException("plainText");
-//			
-//			if (Key == null || Key.Length <= 0)
-//				throw new ArgumentNullException("Key");
-//			
-//			if (IV == null || IV.Length <= 0)
-//				throw new ArgumentNullException("Key");
-//			
-//			byte[] encrypted;
-//
-//			// Create an Aes object
-//			// with the specified key and IV.
-//			using (Aes aesAlg = Aes.Create())
-//			{
-//				aesAlg.Key = Key;
-//				aesAlg.IV = IV;
-//
-//				// Create a decrytor to perform the stream transform.
-//				ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
-//
-//				// Create the streams used for encryption.
-//				using (MemoryStream msEncrypt = new MemoryStream())
-//				{
-//					using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
-//					{
-//						using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
-//						{
-//							//Write all data to the stream.
-//							swEncrypt.Write(plainText);
-//						}
-//						encrypted = msEncrypt.ToArray();
-//					}
-//				}
-//			}
-//
-//			// Return the encrypted bytes from the memory stream.
-//			return encrypted;
-//		}
-
 		/// <summary>
 		/// Shuffle the specified list.
 		/// </summary>
