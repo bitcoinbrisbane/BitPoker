ad63c56b935bbccf4342dfab63f35939caad298a
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2016-03-31 19:51:26 +1000

    Add mock

--- BitPoker.API/Controllers/PlayersController.cs
+++ BitPoker.API/Controllers/PlayersController.cs
@@ -11,9 +11,31 @@ namespace BitPoker.API.Controllers
     {
         public IEnumerable<Models.PlayerInfo> Get()
         {
+            Models.PlayerInfo alice = new Models.PlayerInfo()
+            {
+                BitcoinAddress = "msPJhg9GPzMN6twknwmSQvrUKZbZnk51Tv"
+            };
+
+            Models.PlayerInfo bob = new Models.PlayerInfo()
+            {
+                BitcoinAddress = "mhSW3EUNoVkD1ZQV1ZpnxdRMBjo648enyo"
+            };
+
             List<Models.PlayerInfo> mockPlayers = new List<Models.PlayerInfo>();
+            mockPlayers.Add(alice);
+            mockPlayers.Add(bob);
 
             return mockPlayers;
+        }
+
+        public Models.PlayerInfo Get(String address)
+        {
+            Models.PlayerInfo mock = new Models.PlayerInfo()
+            {
+                BitcoinAddress = address
+            };
+
+            return mock;
         }
     }
 }
