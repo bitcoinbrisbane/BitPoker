a32165799eec2042e0f0750207979e05863b0a83
warpzone81 <luke.bright@gmail.com>
2016-05-01 20:01:41 +1000

    Added MVC home view

---
+++ BitPoker.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BitPoker.Web.MvcApplication" Language="C#" %>
---
+++ BitPoker.Web/controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace BitPoker.Web.controllers
+{
+    public class HomeController : Controller
+    {
+
+        // GET: Messages
+        public ActionResult Index()
+        {
+
+            GetMessages();
+            return View();
+        }
+
+        public void GetMessages()
+        {
+           
+        }
+
+    }
+}
