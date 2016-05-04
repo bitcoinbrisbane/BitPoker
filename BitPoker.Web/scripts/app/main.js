3b785ff84b2e4b9f9e660e497135858d31ec8b17
warpzone81 <luke.bright@gmail.com>
2016-05-05 09:12:39 +1000

    Adding dependencies for SPA

---
+++ BitPoker.Web/scripts/app/main.js
@@ -0,0 +1,20 @@
+﻿require.config({
+    paths: {
+        // Packages
+        'jquery': '/scripts/jquery-2.0.3.min',
+        'kendo': '/scripts/kendo/2013.3.1119/kendo.web.min',
+        'text': '/scripts/text',
+        'router': '/scripts/app/router'
+    },
+    shim: {
+        'kendo': ['jquery']
+    },
+    priority: ['text', 'router', 'app'],
+    jquery: '2.0.3',
+    waitSeconds: 30
+});
+require([
+  'app'
+], function (app) {
+    app.initialize();
+});
