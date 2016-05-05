9e7043a138db9253259960122be652b0add7f337
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2016-05-05 21:49:50 +1000

    Wire up alll method

--- BitPoker.Repository/MockTableRepo.cs
+++ BitPoker.Repository/MockTableRepo.cs
@@ -10,12 +10,15 @@ namespace BitPoker.Repository
         {
             //Return a fake contract
             //{D6D9890D-0CA2-4B5D-AE98-FA4D45EB4363}
-            return new Models.Contracts.Table(2, 10) { Id = new Guid("D6D9890D-0CA2-4B5D-AE98-FA4D45EB4363") };
+            return new Table(2, 10) { Id = new Guid("D6D9890D-0CA2-4B5D-AE98-FA4D45EB4363") };
         }
 
         public IEnumerable<Table> All()
         {
-            throw new NotImplementedException();
+            List<Table> tables = new List<Table>(1);
+            tables.Add(new Table(2, 10) { Id = new Guid("D6D9890D-0CA2-4B5D-AE98-FA4D45EB4363") });
+
+            return tables;
         }
 
         public void Add(Table item)
