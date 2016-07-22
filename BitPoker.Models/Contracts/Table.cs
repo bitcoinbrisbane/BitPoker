370b7320e14576d6d604ecf8bf839e1deee5e3ed
Shocking <lucas@lucascullen.com>
2016-07-22 15:23:42 +1000

    Make build able

--- BitPoker.Models/Contracts/Table.cs
+++ BitPoker.Models/Contracts/Table.cs
@@ -11,7 +11,7 @@ namespace BitPoker.Models.Contracts
 
         public Table()
         {
-            this.Players = new List<TexasHoldemPlayer>(maxPlayers);
+            this.Players = new List<TexasHoldemPlayer>(10);
         }
 
 		public Table (Int16 minPlayers, Int16 maxPlayers)
