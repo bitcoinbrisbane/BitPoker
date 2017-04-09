f22ba8762250e8acc8a0593bdc7800eaf914ee11
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2017-04-09 20:17:44 +1000

    Work on HTML views

--- BitPoker.Owin.RestHost/Program.cs
+++ BitPoker.Owin.RestHost/Program.cs
@@ -15,34 +15,34 @@ namespace BitPoker.Owin.RestHost
                 args[0] = System.Configuration.ConfigurationManager.AppSettings["baseurl"];
             }
 
-            foreach (String baseAddress in args)
-            {
-                //Seems to fail on Mac
-                Task.Run(() =>
-                {
-                    // Start OWIN host 
-                    using (WebApp.Start<Startup>(url: baseAddress))
-                    {
-                        Console.WriteLine("Server running at {0} - press Enter to quit. ", baseAddress);
-                    }
-                });
-            }
-
-
-            //if (!String.IsNullOrEmpty(args[0]))
+            //foreach (String baseAddress in args)
             //{
-            //	// Start OWIN host 
-            //	using (WebApp.Start<Startup>(url: args[0]))
-            //	{
-            //		Console.WriteLine("Server running at {0} - press Enter to quit. ", args[0]);
-            //		Console.ReadLine();
-            //	}
+            //    //Seems to fail on Mac
+            //    Task.Run(() =>
+            //    {
+            //        //Start OWIN host
+            //        using (WebApp.Start<Startup>(url: baseAddress))
+            //        {
+            //            Console.WriteLine("Server running at {0} - press Enter to quit. ", baseAddress);
+            //        }
+            //    });
             //}
-            //else
-            //{
-            //	Console.WriteLine("No base url config setting found");
 
-            //         }
+
+            if (!String.IsNullOrEmpty(args[0]))
+            {
+                // Start OWIN host 
+                using (WebApp.Start<Startup>(url: args[0]))
+                {
+                    Console.WriteLine("Server running at {0} - press Enter to quit. ", args[0]);
+                    Console.ReadLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("No base url config setting found");
+
+            }
 
             Console.ReadLine();
         }
