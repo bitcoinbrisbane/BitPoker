6f3940b0097be6a73f7485d40064b7c08bcff837
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2017-05-07 20:45:40 +1000

    Add API controller

--- BitPoker.Controllers.Rest/AIActionController.cs
+++ BitPoker.Controllers.Rest/AIActionController.cs
@@ -5,36 +5,38 @@ using System.Linq;
 
 namespace BitPoker.Controllers.Rest
 {
-    public class ActionController : BaseController
+    public class AIActionController : BaseController, IActionController
     {
-		public BitPoker.Repository.IHandRepository HandRepo { get; set; }
+	public BitPoker.Repository.IHandRepository HandRepo { get; set; }
 
-		public BitPoker.Repository.IMessagesRepository MessageRepo { get; set; }
+	public BitPoker.Repository.IMessagesRepository MessageRepo { get; set; }
 
-		public ActionController()
+	public AIActionController()
+	{
+		base.PrivateKey = System.Configuration.ConfigurationManager.AppSettings["BitcoinPrivateKey"];
+	}
+
+	[HttpGet]
+	public IEnumerable<BitPoker.Models.Messages.ActionMessage> Get(string id)
+	{
+		return MessageRepo.All().Where(m => m.HandId.ToString() == id);
+	}
+
+	[HttpPost]
+	public void Post(BitPoker.Models.Messages.ActionMessage message)
+	{
+		if (base.Verify(message) == true)
 		{
-			base.PrivateKey = System.Configuration.ConfigurationManager.AppSettings["BitcoinPrivateKey"];
-		}
+			Models.Hand hand = HandRepo.All().Single(h => h.Id.ToString() == message.HandId.ToString());
 
-		[HttpGet]
-		public IEnumerable<BitPoker.Models.Messages.ActionMessage> Get(string id)
-		{
-			return MessageRepo.All().Where(m => m.HandId.ToString() == id);
-		}
-
-		[HttpPost]
-		public void Post(BitPoker.Models.Messages.ActionMessage message)
-		{
-			if (base.Verify(message) == true)
+			if (hand != null)
 			{
-				Models.Hand hand = HandRepo.All().Single(h => h.Id.ToString() == message.HandId.ToString());
-
-				if (hand != null)
-				{
-					BitPoker.Models.Players.IPlayer player1 = new 
-					BitPoker.Logic.GameMechanics.TwoPlayersTexasHoldemGame holdem = new BitPoker.Logic.GameMechanics.TwoPlayersTexasHoldemGame();
-				}
+				BitPoker.Models.Players.IPlayerLogic player1 = new BitPoker.Models.TexasHoldemPlayer();
+				BitPoker.Models.Players.IPlayerLogic player2 = new BitPoker.Models.TexasHoldemPlayer();
+					
+				BitPoker.Logic.GameMechanics.TwoPlayersTexasHoldemGame holdem = new BitPoker.Logic.GameMechanics.TwoPlayersTexasHoldemGame(player1, player2);
 			}
 		}
+	}
     }
 }
